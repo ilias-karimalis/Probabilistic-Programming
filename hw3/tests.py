680708b43f4cbef33491a53ac889b67713eeb31c
Ilias Karimalis <iliaskarimalis@outlook.com>
2021-11-09 16:27:33 -0800

    Fixed HW3 testing code

--- hw3/tests.py
+++ hw3/tests.py
@@ -19,18 +19,18 @@ epsilon = [0.1, 0.1, 0, 0, 0.001]
 for i in range(3, 5):
 
     # Importance Sampling
-    # print(f"Starting Importance Sampling for Program {i}")
-    # is_args = {
-    #     "labels": labels[i],
-    #     "evaluator": "ImportanceSampling",
-    #     "max_time": max_time,
-    #     "n_bins": bins[i-1],
-    #     "bool_res?": True if i in [3, 4] else False,
-    #     "program_name": f"Program{i}",
-    #     "use_weights?": True,
-    #     "ast": daphne(['desugar', '-i', f'../hw3/programs/{i}.daphne'])
-    # }
-    # run_benchmark(is_args)
+    print(f"Starting Importance Sampling for Program {i}")
+    is_args = {
+        "labels": labels[i],
+        "evaluator": "ImportanceSampling",
+        "max_time": max_time,
+        "n_bins": bins[i-1],
+        "bool_res?": True if i in [3, 4] else False,
+        "program_name": f"Program{i}",
+        "use_weights?": True,
+        "ast": daphne(['desugar', '-i', f'../hw3/programs/{i}.daphne'])
+    }
+    run_benchmark(is_args)
 
     # MH in Gibbs
     print(f"Starting MH in Gibbs for Program {i}")
@@ -47,19 +47,19 @@ for i in range(3, 5):
     run_benchmark(mh_args)
 
     # Hamiltonian Monte Carlo
-    # if i in [1, 2, 5]:
-    #     print(f"Starting HMC for Program {i}")
-    #     hmc_args = {
-    #         "labels": labels[i],
-    #         "evaluator": "HMC",
-    #         "max_time": max_time,
-    #         "n_bins": bins[i - 1],
-    #         "bool_res?": False,
-    #         "program_name": f"Program{i}",
-    #         "use_weights?": False,
-    #         "graph": daphne(['graph', '-i', f'../hw3/programs/{i}.daphne']),
-    #         "num_leaps": num_leaps[i - 1],
-    #         "epsilon": epsilon[i - 1],
-    #         "burnin": burnin[i - 1]
-    #     }
-    #     run_benchmark(hmc_args)
+    if i in [1, 2, 5]:
+        print(f"Starting HMC for Program {i}")
+        hmc_args = {
+            "labels": labels[i],
+            "evaluator": "HMC",
+            "max_time": max_time,
+            "n_bins": bins[i - 1],
+            "bool_res?": False,
+            "program_name": f"Program{i}",
+            "use_weights?": False,
+            "graph": daphne(['graph', '-i', f'../hw3/programs/{i}.daphne']),
+            "num_leaps": num_leaps[i - 1],
+            "epsilon": epsilon[i - 1],
+            "burnin": burnin[i - 1]
+        }
+        run_benchmark(hmc_args)
